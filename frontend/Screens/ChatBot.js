54e17c715144e3b4734cb351d20b2e2b2876ebfd
Manikanta891 <143416527+Manikanta891@users.noreply.github.com>
2025-04-06 15:57:36 +0530

    ChatBot.js

--- frontend/Screens/ChatBot.js
+++ frontend/Screens/ChatBot.js
@@ -54,7 +54,7 @@ export default function ChatScreen() {
     const isVideoQuery = chatInput.toLowerCase().includes("video");
     if (isVideoQuery) {
       try {
-        const response = await fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${encodeURIComponent(chatInput)}&key=AIzaSyBs2hEx1YyzBznIZdlf-CoWAjxwFROMarQ`);
+        const response = await fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${encodeURIComponent(chatInput)}&key=process.env.chatkey`);
         const data = await response.json();
         if (data.items && data.items.length > 0) {
           const videoId = data.items[0].id.videoId;
